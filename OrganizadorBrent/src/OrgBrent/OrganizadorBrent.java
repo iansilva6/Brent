ce018e93a86ff59cf5018aa51c28aa57c4fa47e8
victorcarity <victorcfsc@dcomp.ufs.br>
2016-10-22 12:14:58 -0200

    Não lembro porque não pode chamar buf.flip() quando o .db esta vazio. (ERRO)

--- OrganizadorBrent/src/OrgBrent/OrganizadorBrent.java
+++ OrganizadorBrent/src/OrgBrent/OrganizadorBrent.java
@@ -19,7 +19,8 @@ public class OrganizadorBrent implements IFileOrganizer {
 
     //atributos
     private FileChannel canal;
-    private static final long SIZE = 8478101;//É PRIMO!
+    //private static final long SIZE = 8478101;//É PRIMO!
+    private static final long SIZE = 7;//tamanho de teste
 
     //construtor
     public OrganizadorBrent(String pathToFile) throws FileNotFoundException {
@@ -30,9 +31,103 @@ public class OrganizadorBrent implements IFileOrganizer {
 
     }
 
+    //Métodos de Organização Sequencial de Brent
     @Override
-    public boolean putReg(Aluno p) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean putReg(Aluno a) {
+        long pos = (a.getMatric() % SIZE);
+        ByteBuffer buf = ByteBuffer.allocate(Aluno.LENGTH);
+        try {
+            this.canal.read(buf, pos * Aluno.LENGTH);
+        } catch (IOException ex) {
+            return false;
+        }
+        buf.flip();
+        Aluno b = new Aluno(buf);
+        if (b.getMatric() == 0) {
+            try {
+                this.canal.write(a.getBuffer(), pos * Aluno.LENGTH);
+            } catch (IOException ex) {
+                return false;
+            }
+        } else {
+            //caso 1, calcula o incremento do que vai entrar, e procura uma posição livre
+            long incEntrar = (((a.getMatric()) % (SIZE - 2)) + 1);
+            long posicaoEntradaC1 = -1;
+            long custo1 = 0;
+            for (long i = pos * Aluno.LENGTH; i > -1; i += (Aluno.LENGTH
+                    * incEntrar)) {
+                if (i >= SIZE * Aluno.LENGTH) {
+                    i = i - SIZE * Aluno.LENGTH;
+                }
+                ByteBuffer buf2 = ByteBuffer.allocate(4);
+                try {
+                    this.canal.read(buf2, i);
+                } catch (IOException ex) {
+                    Logger.getLogger(OrganizadorBrent.class.getName()).log(
+                            Level.SEVERE, null, ex);
+                    return false;
+                }
+                buf2.flip();
+                int n = buf2.getInt();
+                if (n == 0 || n == -1) {
+                    posicaoEntradaC1 = i;
+                    i = -1;
+                    break;
+                } else {
+                    custo1++;
+                }
+            }
+
+            //caso2, calcula incremento que já está lá, e procura uma pos livre
+            long incElenPos = (((b.getMatric()) % (SIZE - 2)) + 1);
+            long posicaoEntradaC2 = -1;
+            long custo2 = 0;
+            for (long i = pos * Aluno.LENGTH; i > -1; i += Aluno.LENGTH
+                    * incElenPos) {
+                if (i >= SIZE * Aluno.LENGTH) {
+                    i = i - SIZE * Aluno.LENGTH;
+                }
+                ByteBuffer buf2 = ByteBuffer.allocate(4);
+                try {
+                    this.canal.read(buf2, i);
+                } catch (IOException ex) {
+                    Logger.getLogger(OrganizadorBrent.class.getName()).log(
+                            Level.SEVERE, null, ex);
+                    return false;
+                }
+                buf2.flip();
+                int n = buf2.getInt();
+                if ((n == 0 || n == -1)) {
+                    posicaoEntradaC2 = i;
+                    i = -1;
+                    break;
+                } else {
+                    custo2++;
+                }
+            }
+            if (custo1 <= custo2) {
+                try {
+                    this.canal.write(a.getBuffer(), posicaoEntradaC1);
+                } catch (IOException ex) {
+                    Logger.getLogger(OrganizadorBrent.class.getName()).log(
+                            Level.SEVERE, null, ex);
+                    return false;
+                }
+            } else {
+                if (custo2 < custo1) {
+                    try {
+                        this.canal.write(a.getBuffer(), pos * Aluno.LENGTH);
+                        this.canal.write(b.getBuffer(), posicaoEntradaC2);
+                    } catch (IOException ex) {
+                        Logger.getLogger(OrganizadorBrent.class.getName()).log(
+                                Level.SEVERE, null, ex);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -43,6 +138,14 @@ public class OrganizadorBrent implements IFileOrganizer {
     @Override
     public Aluno delReg(int matric) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private long getHash(int matric) {
+        return (matric % SIZE);
+    }
+
+    private long getInc(int matric) {
+        return ((matric % (SIZE - 2)) + 1);
     }
 
 }
